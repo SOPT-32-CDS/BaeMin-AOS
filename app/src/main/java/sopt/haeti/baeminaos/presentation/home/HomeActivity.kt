f5739801379c5eb4ad30aa3043ae233b4ebfed0d
taeheeL <t9h9@soongsil.ac.kr>
2023-05-15 23:48:30 +0900

    #1 / [MOVE] MainActivity -> HomeActivity

--- app/src/main/java/sopt/haeti/baeminaos/presentation/home/HomeActivity.kt
+++ app/src/main/java/sopt/haeti/baeminaos/presentation/home/HomeActivity.kt
@@ -1,9 +1,10 @@
-package sopt.haeti.baeminaos
+package sopt.haeti.baeminaos.presentation.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import sopt.haeti.baeminaos.R
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
