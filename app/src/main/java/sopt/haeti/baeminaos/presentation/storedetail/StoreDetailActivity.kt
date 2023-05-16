e42f6d96790b50386215650f95164a88b7d63c66
taeheeL <t9h9@soongsil.ac.kr>
2023-05-16 22:22:27 +0900

    #1 / [FIX] StoreDetailActivity 이름 수정

--- app/src/main/java/sopt/haeti/baeminaos/presentation/storedetail/StoreDetailActivity.kt
+++ app/src/main/java/sopt/haeti/baeminaos/presentation/storedetail/StoreDetailActivity.kt
@@ -5,7 +5,7 @@ import sopt.haeti.baeminaos.R
 import sopt.haeti.baeminaos.databinding.ActivityHomeBinding
 import sopt.haeti.baeminaos.util.base.BindingActivity
 
-class StroeDetailActivity : BindingActivity<ActivityHomeBinding>(R.layout.activity_home) {
+class StoreDetailActivity : BindingActivity<ActivityHomeBinding>(R.layout.activity_home) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
