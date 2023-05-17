fbcf21efb7cb29c27930ab02d9f17aac3ef3e505
Sangho Kim <marchbreeze03@gmail.com>
2023-05-17 18:22:02 +0900

    #4 / [FEAT] toolbar 아이템 클릭 시 액션 설정
    툴바의 아이템 클릭 시 Timber로 로그 작성되는 기능 정도만 추가

--- app/src/main/java/sopt/haeti/baeminaos/presentation/cart/CartActivity.kt
+++ app/src/main/java/sopt/haeti/baeminaos/presentation/cart/CartActivity.kt
@@ -2,14 +2,18 @@ package sopt.haeti.baeminaos.presentation.cart
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import sopt.haeti.baeminaos.R
 import sopt.haeti.baeminaos.databinding.ActivityCartBinding
 import sopt.haeti.baeminaos.util.base.BindingActivity
+import timber.log.Timber
 
 class CartActivity : BindingActivity<ActivityCartBinding>(R.layout.activity_cart) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        Timber.plant(Timber.DebugTree())
 
         // 툴바 설정
         setSupportActionBar(binding.toolbarCart)
@@ -19,8 +23,24 @@ class CartActivity : BindingActivity<ActivityCartBinding>(R.layout.activity_cart
         supportActionBar!!.setHomeAsUpIndicator(R.drawable.ic_back)
     }
 
+    // 툴바에 아이템 설정
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_cart_toolbar, menu)
+        return true
+    }
+
+    // 툴바 아이템 클릭 시 액션 설정
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_cart_toolbar_home -> {
+                Timber.d("툴바 메인화면 버튼 클릭")
+            }
+            R.id.menu_cart_toolbar_person_plus -> {
+                //도움말 버튼 눌렀을 때
+                Timber.d("툴바 친구추가 버튼 클릭")
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
         return true
     }
 }
