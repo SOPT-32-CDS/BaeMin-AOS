72e35f50324d9cfd6ffbb003c32cb8e4a97084e2
Sangho Kim <marchbreeze03@gmail.com>
2023-05-17 16:06:20 +0900

    #4/ [UI] set toolbar for cart activity
    cart activity의 맨 위 툴바 설정

--- app/src/main/java/sopt/haeti/baeminaos/presentation/cart/CartActivity.kt
+++ app/src/main/java/sopt/haeti/baeminaos/presentation/cart/CartActivity.kt
@@ -1,13 +1,26 @@
 package sopt.haeti.baeminaos.presentation.cart
 
 import android.os.Bundle
+import android.view.Menu
 import sopt.haeti.baeminaos.R
-import sopt.haeti.baeminaos.databinding.ActivityHomeBinding
+import sopt.haeti.baeminaos.databinding.ActivityCartBinding
 import sopt.haeti.baeminaos.util.base.BindingActivity
 
-class CartActivity : BindingActivity<ActivityHomeBinding>(R.layout.activity_home) {
+class CartActivity : BindingActivity<ActivityCartBinding>(R.layout.activity_cart) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        // 툴바 설정
+        setSupportActionBar(binding.toolbarCart)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        // 툴바의 뒤로가기 버튼 설정
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.setHomeAsUpIndicator(R.drawable.ic_back)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_cart_toolbar, menu)
+        return true
     }
 }
