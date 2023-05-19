056e19c20da88e0d2301cab8078a237d5b87ba49
Jihyeon An <jh011007@naver.com>
2023-05-20 00:59:16 +0900

    Add : 메뉴 이미지/이름/상세/가격 뷰 만들기

--- app/src/main/java/sopt/haeti/baeminaos/presentation/storedetail/StoreDetailActivity.kt
+++ app/src/main/java/sopt/haeti/baeminaos/presentation/storedetail/StoreDetailActivity.kt
@@ -1,13 +1,46 @@
 package sopt.haeti.baeminaos.presentation.storedetail
 
 import android.os.Bundle
+import android.widget.LinearLayout.VERTICAL
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import sopt.haeti.baeminaos.R
+import sopt.haeti.baeminaos.data.local.StoreDetailOptionDataClass
 import sopt.haeti.baeminaos.databinding.ActivityHomeBinding
+import sopt.haeti.baeminaos.databinding.ActivityStoreDetailBinding
+import sopt.haeti.baeminaos.presentation.storedetail.adapter.OptionRVAdapter
 import sopt.haeti.baeminaos.util.base.BindingActivity
 
-class StoreDetailActivity : BindingActivity<ActivityHomeBinding>(R.layout.activity_home) {
+class StoreDetailActivity : BindingActivity<ActivityStoreDetailBinding>(R.layout.activity_store_detail) {
+
+    private lateinit var optionRVAdapter : OptionRVAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        val data = mutableListOf(
+            StoreDetailOptionDataClass(
+                "옵션 명",
+                "최대 6개 추가"
+            ),
+            StoreDetailOptionDataClass(
+                "옵션 명",
+                "최대 6개 추가"
+            ),
+            StoreDetailOptionDataClass(
+                "옵션 명",
+                "최대 6개 추가"
+            ),
+        )
+
+        val decoration = DividerItemDecoration(this, VERTICAL)
+
+        optionRVAdapter = OptionRVAdapter()
+        optionRVAdapter.submitList(data)
+        binding.rvOption.apply {
+            layoutManager = LinearLayoutManager(this@StoreDetailActivity)
+            adapter = optionRVAdapter
+            addItemDecoration(decoration)
+        }
     }
 }
