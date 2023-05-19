7d2394f04d8b595be582b77ea42ddae996b473d5
Sangho Kim <marchbreeze03@gmail.com>
2023-05-20 02:04:51 +0900

    #7 / [FEAT] 총금액 계산 기능 추가
    아이템1의 수량에 따라 총 주문금액 변경되는 기능 추가

--- app/src/main/java/sopt/haeti/baeminaos/presentation/cart/CartActivity.kt
+++ app/src/main/java/sopt/haeti/baeminaos/presentation/cart/CartActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import sopt.haeti.baeminaos.R
+import sopt.haeti.baeminaos.data.local.CartItemData
 import sopt.haeti.baeminaos.databinding.ActivityCartBinding
 import sopt.haeti.baeminaos.util.base.BindingActivity
 import timber.log.Timber
@@ -13,12 +14,36 @@ class CartActivity : BindingActivity<ActivityCartBinding>(R.layout.activity_cart
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        var itemOneCount = 0
+        var itemOneTotalPrice = 0
+
         // 툴바 설정
         setSupportActionBar(binding.toolbarCart)
         supportActionBar?.setDisplayShowTitleEnabled(false)
         // 툴바의 뒤로가기 버튼 설정
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
         supportActionBar!!.setHomeAsUpIndicator(R.drawable.ic_back)
+
+        // 임시 데이터클래스 & 데이터 설정
+        val mockCartItemData = CartItemData(1,"모듬초밥", 12000, "연어 10피스", 2)
+        // 아이템 데이터 삽입
+        with(binding) {
+            tvCartItem1Title.text = mockCartItemData.itemName
+            tvCartItem1Option.text = mockCartItemData.options
+            itemOneCount = mockCartItemData.count
+            tvCartItem1Number.text = itemOneCount.toString()
+            itemOneTotalPrice = mockCartItemData.price * mockCartItemData.count
+            tvCartItem1Price.text = itemOneTotalPrice.toString() + "원"
+        }
+
+        // 총 주문금액 변경
+        with(binding) {
+            val itemTotalPrice = 26600 + itemOneTotalPrice
+            tvCartDetailPrice.text = itemTotalPrice.toString() + "원"
+            val itemTotalPriceWithTip = itemTotalPrice + 2000
+            tvCartDetailTotalPrice.text = itemTotalPriceWithTip.toString() + "원"
+            tvCartPurchasePrice.text = itemTotalPriceWithTip.toString() + "원"
+        }
     }
 
     // 툴바에 아이템 설정
