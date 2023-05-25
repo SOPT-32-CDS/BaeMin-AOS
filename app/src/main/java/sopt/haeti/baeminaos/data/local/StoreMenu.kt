2c8f3aba2ee951d9b71cd9673eb9f9df523b60b8
HaeTi <t9h9@naver.com>
2023-05-26 02:34:12 +0900

    [CHORE] data class 합치기

--- app/src/main/java/sopt/haeti/baeminaos/data/local/StoreMenu.kt
+++ app/src/main/java/sopt/haeti/baeminaos/data/local/StoreMenu.kt
@@ -2,5 +2,13 @@ package sopt.haeti.baeminaos.data.local
 
 data class StoreMenu(
     var menuName: String = "",
-    var isExpanded : Boolean = false,
+    var isExpanded: Boolean = false,
+    val storeMenuDetail: List<StoreMenuDetail>
+)
+
+data class StoreMenuDetail(
+    val menuDetailName: String,
+    val menuDescription: String,
+    val menuPrice: Int,
+    val menuImage: String,
 )
--- app/src/main/java/sopt/haeti/baeminaos/data/local/StoreMenuDetail.kt
+++
@@ -1,8 +0,0 @@
-package sopt.haeti.baeminaos.data.local
-
-data class StoreMenuDetail(
-    val menuDetailName: String,
-    val menuDescription: String,
-    val menuPrice: Int,
-    val menuImage: String,
-)
