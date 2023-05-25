73deb60de195b309d2545d409902d9c5f9c72f51
HaeTi <t9h9@naver.com>
2023-05-25 23:29:59 +0900

    [ADD] StoreActivity 추가

--- app/src/main/java/sopt/haeti/baeminaos/presentation/store/StoreActivity.kt
+++ app/src/main/java/sopt/haeti/baeminaos/presentation/store/StoreActivity.kt
@@ -3,9 +3,10 @@ package sopt.haeti.baeminaos.presentation.store
 import android.os.Bundle
 import sopt.haeti.baeminaos.R
 import sopt.haeti.baeminaos.databinding.ActivityHomeBinding
+import sopt.haeti.baeminaos.databinding.ActivityStoreBinding
 import sopt.haeti.baeminaos.util.base.BindingActivity
 
-class StoreActivity : BindingActivity<ActivityHomeBinding>(R.layout.activity_home) {
+class StoreActivity : BindingActivity<ActivityStoreBinding>(R.layout.activity_store) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
