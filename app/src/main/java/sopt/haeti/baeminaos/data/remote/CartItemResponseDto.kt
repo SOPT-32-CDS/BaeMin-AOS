686f6f479f83a626af9b98b537d697e8e177d71a
Sangho Kim <marchbreeze03@gmail.com>
2023-05-26 01:26:11 +0900

    #14 / [ADD] CartItem 받아오는 Dto에 SerialName 설정

--- app/src/main/java/sopt/haeti/baeminaos/data/remote/CartItemResponseDto.kt
+++ app/src/main/java/sopt/haeti/baeminaos/data/remote/CartItemResponseDto.kt
@@ -1,32 +1,49 @@
 package sopt.haeti.baeminaos.data.remote
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
-data class CartItemDto(
+data class CartItemResponseDto(
+    @SerialName("status")
     val status: Int,
+    @SerialName("message")
     val message: String,
+    @SerialName("data")
     val data: List<Cart>
 ) {
     @Serializable
     data class Cart(
+        @SerialName("cart_id")
         val cartId: Int,
+        @SerialName("total_price")
         val totalPrice: Int,
+        @SerialName("delivery_fee")
         val deliveryFee: Int,
+        @SerialName("cart_store_list")
         val cartStoreList: List<CartStore>
     ) {
         @Serializable
         data class CartStore(
+            @SerialName("cart_store_id")
             val cartStoreId: Int,
+            @SerialName("name")
             val name: String,
+            @SerialName("image")
             val image: String,
+            @SerialName("cart_item_list")
             val cartItemList: List<CartItem>
         ) {
             @Serializable
             data class CartItem(
+                @SerialName("cart_item_id")
                 val cartItemId: Int,
+                @SerialName("name")
                 val name: String,
+                @SerialName("total_price")
                 val totalPrice: Int,
+                @SerialName("options")
                 val options: String,
+                @SerialName("count")
                 val count: Int
             )
         }
