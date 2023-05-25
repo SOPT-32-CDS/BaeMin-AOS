d604f27e45da6b464a0e609b42be384311e16438
HaeTi <t9h9@naver.com>
2023-05-25 23:29:37 +0900

    [ADD] StoreMenuDetail Dt

---
+++ app/src/main/java/sopt/haeti/baeminaos/data/local/StoreMenuDetail.kt
@@ -0,0 +1,8 @@
+package sopt.haeti.baeminaos.data.local
+
+data class StoreMenuDetail(
+    val menuDetailName: String,
+    val menuDescription: String,
+    val menuPrice: Int,
+    val menuImage: String,
+)
