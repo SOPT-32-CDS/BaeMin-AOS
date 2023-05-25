db3d719c329b47207f518946d525567669ce8547
Sangho Kim <marchbreeze03@gmail.com>
2023-05-26 03:51:33 +0900

    #14 / [FEAT] GET CartItem의 interceptor 추가

--- app/src/main/java/sopt/haeti/baeminaos/module/CartApiFactory.kt
+++ app/src/main/java/sopt/haeti/baeminaos/module/CartApiFactory.kt
@@ -1,9 +1,10 @@
 package sopt.haeti.baeminaos.module
 
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
-import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import sopt.haeti.baeminaos.BuildConfig
 import sopt.haeti.baeminaos.data.service.CartService
@@ -11,11 +12,18 @@ import sopt.haeti.baeminaos.data.service.CartService
 object CartApiFactory {
     private const val BASE_URL = BuildConfig.BASE_URL
 
-    @OptIn(ExperimentalSerializationApi::class)
+    private val client by lazy {
+        OkHttpClient.Builder().addInterceptor(HttpLoggingInterceptor().apply {
+            level =
+                if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.NONE
+        }).build()
+    }
+
     val retrofit: Retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+            .client(client)
             .build()
     }
 
