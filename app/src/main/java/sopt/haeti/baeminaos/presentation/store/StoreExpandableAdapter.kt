fc4fde63f9f3e1cf753d1a7191dfb7f15af7ba5a
HaeTi <t9h9@naver.com>
2023-05-26 17:19:40 +0900

    [CHORE] 서버통신에 맞게 어뎁터 변경

--- app/src/main/java/sopt/haeti/baeminaos/presentation/store/StoreExpandableAdapter.kt
+++ app/src/main/java/sopt/haeti/baeminaos/presentation/store/StoreExpandableAdapter.kt
@@ -4,17 +4,20 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import sopt.haeti.baeminaos.data.local.StoreMenu
+import sopt.haeti.baeminaos.data.remote.StoreDetailResponseDto
 import sopt.haeti.baeminaos.databinding.ItemStoreMenuBinding
 import sopt.haeti.baeminaos.util.ItemDiffCallback
 
 class StoreExpandableAdapter(
-    context: Context, private val storeMenu: List<StoreMenu>
-) : ListAdapter<StoreMenu, StoreExpandableAdapter.StoreMenuViewHolder>(
-    ItemDiffCallback<StoreMenu>(
-        onItemsTheSame = { old, new -> old.menuName == new.menuName },
+    context: Context, private val storeMenu: List<StoreDetailResponseDto.Data.MenuCategory>
+) : ListAdapter<StoreDetailResponseDto.Data.MenuCategory, StoreExpandableAdapter.StoreMenuViewHolder>(
+    ItemDiffCallback<StoreDetailResponseDto.Data.MenuCategory>(
+        onItemsTheSame = { old, new -> old.id == new.id },
         onContentsTheSame = { old, new -> old == new })
 ) {
 
@@ -28,7 +31,7 @@ class StoreExpandableAdapter(
 
     override fun onBindViewHolder(holder: StoreMenuViewHolder, position: Int) {
         val context = holder.itemView.context
-        val innerAdapter = StoreMenuInnerAdapter(storeMenu[position].storeMenuDetail, context)
+        val innerAdapter = StoreMenuInnerAdapter(storeMenu[position].menus, context)
         holder.onBind(getItem(position), innerAdapter)
     }
 
@@ -36,11 +39,19 @@ class StoreExpandableAdapter(
     class StoreMenuViewHolder(
         private val binding: ItemStoreMenuBinding
     ) : RecyclerView.ViewHolder(binding.root) {
-        fun onBind(item: StoreMenu, innerAdapter: StoreMenuInnerAdapter) {
-            binding.tvMenu.text = item.menuName
+        fun onBind(
+            item: StoreDetailResponseDto.Data.MenuCategory,
+            innerAdapter: StoreMenuInnerAdapter
+        ) {
+            val decoration =
+                DividerItemDecoration(binding.rvMenuDetail.context, LinearLayout.VERTICAL)
+
+            binding.rvMenuDetail.layoutManager = LinearLayoutManager(binding.rvMenuDetail.context)
+            binding.tvMenu.text = item.name
             binding.rvMenuDetail.adapter = innerAdapter
+            binding.rvMenuDetail.addItemDecoration(decoration)
             binding.executePendingBindings()
-            innerAdapter.submitList(item.storeMenuDetail)
+            innerAdapter.submitList(item.menus)
 
             binding.root.setOnClickListener {
                 val show = toggleLayout(!item.isExpanded, binding.ivArrow, binding.rvMenuDetail)
--- app/src/main/java/sopt/haeti/baeminaos/presentation/store/StoreMenuInnerAdapter.kt
+++ app/src/main/java/sopt/haeti/baeminaos/presentation/store/StoreMenuInnerAdapter.kt
@@ -7,19 +7,20 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import sopt.haeti.baeminaos.R
-import sopt.haeti.baeminaos.data.local.StoreMenuDetail
+import sopt.haeti.baeminaos.data.remote.StoreDetailResponseDto
 import sopt.haeti.baeminaos.databinding.ItemStoreMenuDetailBinding
 import sopt.haeti.baeminaos.util.ItemDiffCallback
 import java.text.DecimalFormat
 
 class StoreMenuInnerAdapter(
-    private val storeMenuInnerList: List<StoreMenuDetail>,
+    private val storeMenuInnerList: List<StoreDetailResponseDto.Data.MenuCategory.Menu>,
     context: Context
 ) :
-    ListAdapter<StoreMenuDetail, StoreMenuInnerAdapter.StoreMenuInnerViewHolder>(ItemDiffCallback<StoreMenuDetail>(
-        onItemsTheSame = { old, new -> old.menuDetailName == new.menuDetailName },
-        onContentsTheSame = { old, new -> old == new }
-    )) {
+    ListAdapter<StoreDetailResponseDto.Data.MenuCategory.Menu, StoreMenuInnerAdapter.StoreMenuInnerViewHolder>(
+        ItemDiffCallback<StoreDetailResponseDto.Data.MenuCategory.Menu>(
+            onItemsTheSame = { old, new -> old.id == new.id },
+            onContentsTheSame = { old, new -> old == new }
+        )) {
 
     private val inflater by lazy { LayoutInflater.from(context) }
 
@@ -36,14 +37,40 @@ class StoreMenuInnerAdapter(
 
     class StoreMenuInnerViewHolder(private val binding: ItemStoreMenuDetailBinding) :
         RecyclerView.ViewHolder(binding.root) {
-        fun onBind(item: StoreMenuDetail) {
-            binding.tvMenuDetailName.text = item.menuDetailName
+        fun onBind(item: StoreDetailResponseDto.Data.MenuCategory.Menu) {
+            binding.tvMenuDetailName.text = truncateText(item.name, 14)
             binding.ivBestMenu.load(R.drawable.ic_best_menu)
-            binding.tvBestMenuDesc.text = item.menuDescription
-            binding.tvBestMenuPrice.text = moneyFormat(item.menuPrice)
-            binding.ivMenuDetail.load(item.menuImage)
+            binding.tvBestMenuDesc.text = wrapText(item.description, 28)
+            binding.tvBestMenuPrice.text = moneyFormat(item.base_price)
+            binding.ivMenuDetail.load(item.image)
             binding.executePendingBindings()
 
+        }
+
+        private fun truncateText(text: String, maxLength: Int): String {
+            return if (text.length > maxLength) {
+                text.substring(0, maxLength) + "..."
+            } else {
+                text
+            }
+        }
+
+        private fun wrapText(text: String, maxChars: Int): String {
+            val words = text.split(" ")
+            val output = StringBuilder()
+            var currentLineLength = 0
+
+            for (word in words) {
+                if (currentLineLength + word.length > maxChars) {
+                    output.append("\n")
+                    currentLineLength = 0
+                }
+                output.append(word)
+                output.append(" ")
+                currentLineLength += word.length + 1
+            }
+
+            return output.toString()
         }
 
         private fun moneyFormat(money: Int): String {
