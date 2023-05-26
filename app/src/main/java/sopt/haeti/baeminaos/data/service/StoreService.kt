dcc5d041bcac0379c0f3a631363ff82a1d217eeb
HaeTi <t9h9@naver.com>
2023-05-26 17:18:22 +0900

    [CHORE] store api 연동

--- app/src/main/java/sopt/haeti/baeminaos/data/service/StoreService.kt
+++ app/src/main/java/sopt/haeti/baeminaos/data/service/StoreService.kt
@@ -1,11 +1,10 @@
 package sopt.haeti.baeminaos.data.service
 
-import retrofit2.Call
 import retrofit2.http.GET
 import sopt.haeti.baeminaos.data.remote.StoreDetailResponseDto
 
-interface StoreDetailService {
+interface StoreService {
 
     @GET("store/1")
-    fun getStoreDetail(): Call<StoreDetailResponseDto>
+    suspend fun getStoreDetail(): StoreDetailResponseDto
 }
